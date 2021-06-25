51c9aaab919ee454c9b5f0ac640e53efd5dd4c92
Alex Malitsky <1457274+amalitsky@users.noreply.github.com>
2021-06-24 23:27:52 -0700

    feat(Counter): Remove Y metrics
    Since it doesnt work anyway

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,30 +1,12 @@
 import './src/styles/index.scss';
 
-import { yMetricsInit } from './src/services/y-metrica';
+import { gAnalyticsInit, IGAnalyticsWindow } from './src/services/g-analytics';
 
 export { wrapPageElement } from './src/gatsby-hooks/wrap-page-element';
 export { wrapRootElement } from './src/gatsby-hooks/wrap-root-element';
 
 export function onClientEntry(): void {
   if (process.env.NODE_ENV === 'production') {
-    yMetricsInit(window, document);
-
-    // eslint-disable-next-line no-extra-parens
-    (window as any).dataLayer = (window as any).dataLayer || [];
-
-    // eslint-disable-next-line no-inner-declarations
-    function gtag(): void {
-      // eslint-disable-next-line no-extra-parens
-      (window as any).dataLayer.push(arguments);
-    }
-
-    // eslint-disable-next-line no-extra-parens
-    (window as any).gtag = gtag;
-
-    // @ts-ignore
-    gtag('js', new Date());
-
-    // @ts-ignore
-    gtag('config', 'G-6G7FL8W2JR');
+    gAnalyticsInit(window as unknown as IGAnalyticsWindow);
   }
 }
