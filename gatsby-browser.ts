72a61defea0998a6fd1af07b82e0f6e9d1d12d5a
Alex Malitsky <1457274+amalitsky@users.noreply.github.com>
2021-06-28 22:28:07 -0700

    feat(G Analytics): Add gtag plugin

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,12 +1,4 @@
 import './src/styles/index.scss';
 
-import { gAnalyticsInit, IGAnalyticsWindow } from './src/services/g-analytics';
-
 export { wrapPageElement } from './src/gatsby-hooks/wrap-page-element';
 export { wrapRootElement } from './src/gatsby-hooks/wrap-root-element';
-
-export function onClientEntry(): void {
-  if (process.env.NODE_ENV === 'production') {
-    gAnalyticsInit(window as unknown as IGAnalyticsWindow);
-  }
-}
--- gatsby.config.ts
+++ gatsby.config.ts
@@ -71,6 +71,26 @@ const plugins = [
         'gatsby-remark-reading-time',
       ],
     },
+  }, {
+    resolve: 'gatsby-plugin-google-gtag',
+    options: {
+      trackingIds: [
+        'G-6G7FL8W2JR',
+      ],
+      // This object gets passed directly to the gtag config command
+      gtagConfig: {
+        optimize_id: 'OPT_CONTAINER_ID',
+        anonymize_ip: true,
+        cookie_expires: 0,
+      },
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
   },
   /* {
     resolve: 'gatsby-plugin-graphql-codegen',
