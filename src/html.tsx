3b3ccc32bed3269dc7793ff06e38b07e04950091
Alex Malitsky <1457274+amalitsky@users.noreply.github.com>
2022-04-08 23:03:07 -0700

    chor(perf): Prefetch google font API calls

--- src/html.tsx
+++ src/html.tsx
@@ -1,53 +1,92 @@
-/** @jsx h */
-
-import {
-  h,
-  VNode,
-  Attributes,
-} from 'preact';
+import React, {
+  HTMLAttributes,
+  ReactElement,
+} from 'react';
 
 interface IHTMLPageProps {
-  htmlAttributes: Attributes,
-  headComponents: VNode[],
-  bodyAttributes: Attributes,
-  preBodyComponents: VNode[],
+  htmlAttributes: HTMLAttributes<any>,
+  headComponents: ReactElement[],
+  bodyAttributes: HTMLAttributes<any>,
+  preBodyComponents: ReactElement[],
   body: string,
-  postBodyComponents: VNode[],
+  postBodyComponents: ReactElement[],
 }
 
-function HTML(props: IHTMLPageProps): VNode {
+const googleApiHostname = 'https://fonts.googleapis.com';
+
+const fontPaths = [
+  'css?family=Source+Sans+Pro:400,500,600,700|Merriweather:700&display=swap',
+  'icon?family=Material+Icons&display=swap',
+];
+
+const fontLinks =
+  <>
+    <link
+      rel = 'preconnect'
+      href = { googleApiHostname }
+    />
+
+    { fontPaths.map((fontPath) => {
+      const fontUrl = `${ googleApiHostname }/${ fontPath }`;
+
+      return <>
+        <link
+          rel = 'preload'
+          as = 'style'
+          href = { fontUrl }
+        />
+
+        <link
+          rel = 'stylesheet'
+          href = { fontUrl }
+          media = 'print'
+          // @ts-ignore
+          onLoad = 'this.media="all"'
+        />
+
+        <noscript>
+          <link
+            rel = 'stylesheet'
+            href = { fontUrl }
+          />
+        </noscript>
+      </>;
+    })}
+  </>;
+
+function HTML(props: IHTMLPageProps): ReactElement {
   return (
     <html { ...props.htmlAttributes }>
       <head>
         <meta charSet = 'utf-8'/>
+
         <meta
           httpEquiv = 'x-ua-compatible'
           content = 'ie=edge'
         />
+
         <meta
           name = 'viewport'
           content = 'width=device-width, initial-scale=1, shrink-to-fit=no'
         />
-        <link
-          rel = 'stylesheet'
-          href = 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,500,600,700|Merriweather:700&display=swap'
-        />
-        <link
-          rel = 'stylesheet'
-          href = 'https://fonts.googleapis.com/icon?family=Material+Icons'
-        />
+
+        { fontLinks }
+
         { props.headComponents }
       </head>
+
       <body { ...props.bodyAttributes }>
         { props.preBodyComponents }
+
         <div
-          style = {{
+          style = { {
             flexGrow: 1,
-          }}
+          } }
           key = 'body'
           id = '___gatsby'
-          dangerouslySetInnerHTML = {{ __html: props.body }}
+          dangerouslySetInnerHTML = { { __html: props.body } }
         />
+
         { props.postBodyComponents }
       </body>
     </html>
